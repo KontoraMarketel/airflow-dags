f586b9a5ed078d281c027b72bc1169a19f5543e6
georgiygusev <georgiy.gus@yandex.ru>
2025-07-16 02:29:15 +0300

    actualize project context

--- dags/wb_api_commission_ingest.py
+++ dags/wb_api_commission_ingest.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 
 from airflow.decorators import dag
 from airflow.providers.apache.kafka.operators.produce import ProduceToTopicOperator
+from airflow.sdk import Variable
 
 default_args = {
     "owner": "airflow",
@@ -17,9 +18,13 @@ default_args = {
 def producer_func(init_date: str):
     logging.info(f"Kafka producer called with init_date: {init_date}")
     task_id = str(uuid.uuid4())
+
+    wb_token = Variable.get("WB_API_TOKEN")  # Make sure this variable exists in Airflow
+
     value = {
         "task_id": task_id,
         "init_date": init_date,
+        "wb_token": wb_token,
     }
     return [
         (task_id.encode("utf-8"), json.dumps(value).encode("utf-8"))
@@ -27,9 +32,9 @@ def producer_func(init_date: str):
 
 
 @dag(
-    "wb_commission_ingest",
+    "wb_ingest",
     default_args=default_args,
-    description="Сбор комиссий с wildberies",
+    description="Запуск инжеста данных из вб",
     schedule="0 1 * * *",
     catchup=False,
     tags=["wildberries", "elt", "ingest"],
@@ -38,14 +43,13 @@ def wb_commision_ingest():
     logging.basicConfig(level=logging.INFO)
 
     kafka_notifier = ProduceToTopicOperator(
-        task_id="start_data_pipine_produce_task_to_kafka",
-        topic="ingest-commission-tasks",
+        task_id="send_ingest_task_to_topic",
+        topic="ingest-tasks",
         producer_function=producer_func,
         kafka_config_id="kafka_default",
         producer_function_kwargs={"init_date": "{{ ts }}"},
 
     )
-
     kafka_notifier
 
 
